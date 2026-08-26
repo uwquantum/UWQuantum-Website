ee86006158a6e1dd61323a6a6f6dcf631c8d3d48
LuisaTu2 <luisa.pegoraro@gmail.com>
2026-08-26 01:51:17 -0400

    [isa] refactoring nav bar and footer

--- scripts/site-components.js
+++ scripts/site-components.js
@@ -130,6 +130,7 @@ class SiteFooterNoWordMark extends HTMLElement {
           <li><a href="game.html">Games</a></li>
           <li><a href="team.html">Team</a></li>
           <li><a href="join.html">Join</a></li>
+          <li><a href="qiskit-fall-fest.html">Qiskit Fall Fest</a></li>
           <li>
             <a href="mailto:uwquantumclub@outlook.com">uwquantumclub@outlook.com</a>
           </li>
